2640dbb643ea23870da9915b8800c6ff97c1c248
Nikita <niklog21@gmail.com>
2022-03-12 11:45:37 +0200

    Changed LanguageSelectorActivity

--- app/src/main/java/com/ioskey/iosboard/ui/language/selector/activity/LanguageSelectorViewModel.kt
+++ app/src/main/java/com/ioskey/iosboard/ui/language/selector/activity/LanguageSelectorViewModel.kt
@@ -4,21 +4,9 @@ import com.ioskey.iosboard.R
 import com.ioskey.iosboard.databinding.ItemLanguageBinding
 import com.ioskey.iosboard.ui.base.BaseViewModel
 import com.ioskey.iosboard.ui.base.createAdapter
-import com.ioskey.iosboard.ui.custom.ItemDecorationWithEnds
 import com.ioskey.iosboard.util.enums.Language
-import com.ioskey.iosboard.util.toPx
 
 class LanguageSelectorViewModel : BaseViewModel() {
-    val itemDecoration = ItemDecorationWithEnds(
-        24.toPx(),
-        5.toPx(),
-        5.toPx(),
-        5.toPx(),
-        5.toPx(),
-        24.toPx(),
-        16.toPx(),
-        16.toPx()
-    )
     val adapter = createAdapter<Language, ItemLanguageBinding>(R.layout.item_language) {
         initItems = Language.values().toList()
         onItemClick = ::handleLanguagePress
