e66886bc22255041cc664bf5138d5a6e4b648eeb
Nikita <niklog21@gmail.com>
2022-03-22 18:18:40 +0200

    Changed themes

--- app/src/main/java/com/ioskey/iosboard/util/AppUtils.kt
+++ app/src/main/java/com/ioskey/iosboard/util/AppUtils.kt
@@ -3,7 +3,12 @@ package com.ioskey.iosboard.util
 import android.text.InputType
 import android.view.inputmethod.EditorInfo
 import androidx.databinding.Observable
+import com.google.gson.Gson
 import com.ioskey.iosboard.NeonApplication
+import com.ioskey.iosboard.data.KeyboardTheme
+import com.ioskey.iosboard.repository.PrefsReporitory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import java.io.File
 
 fun Observable.addOnPropertyChangedCallback(
@@ -23,4 +28,23 @@ fun EditorInfo.isPasswordInputType(): Boolean {
     return (variation == InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)
         || (variation == InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_WEB_PASSWORD)
         || (variation == InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_VARIATION_PASSWORD)
+}
+
+fun getPresetThemes(): List<KeyboardTheme> {
+    val themeList: List<KeyboardTheme>
+    runBlocking(Dispatchers.IO) {
+        val gson = Gson()
+        val assetFolder = "ime/theme"
+        val assets = NeonApplication.instance.assets
+
+        themeList = assets.list(assetFolder)!!
+            .map { assets.open("${assetFolder}/$it").bufferedReader().use { theme -> theme.readText() } }
+            .map { gson.fromJson(it, KeyboardTheme::class.java) }
+            .sortedBy { it.index }
+
+        if (PrefsReporitory.keyboardTheme?.id == null) themeList.forEach {
+            it.isSelected = it.backgroundImagePath == PrefsReporitory.keyboardTheme?.backgroundImagePath
+        }
+    }
+    return themeList
 }
--- app/src/main/java/com/ioskey/iosboard/util/Constants.kt
+++ app/src/main/java/com/ioskey/iosboard/util/Constants.kt
@@ -1,7 +1,13 @@
 package com.ioskey.iosboard.util
 
+import com.ioskey.iosboard.data.KeyboardTheme
+
 const val BUNDLE_THEME_KEY = "theme_key"
 const val BUNDLE_IS_EDITING_THEME_KEY = "is_keyboard_was_editing_key"
 const val BUNDLE_CROPPED_IMAGE_KEY = "cropped_image_key"
 
 const val EXTRA_LAUNCH_SETTINGS = "launch_settings"
+
+val themes: List<KeyboardTheme> by lazy { getPresetThemes() }
+val themesPopular: List<KeyboardTheme> by lazy { themes.take(4) }
+val themesOther: List<KeyboardTheme> by lazy { themes.takeLast(10) }
